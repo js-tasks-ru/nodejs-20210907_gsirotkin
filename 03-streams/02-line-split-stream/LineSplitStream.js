5347e89dfc09322a15db5260640157f93d9f4d28
GeoS74 <gsirotkin@yandex.ru>
2021-09-17 19:45:03 +0500

    solution task 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,29 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.on('end', _ => console.log('end'));
   }
 
+    str = '';
+    
   _transform(chunk, encoding, callback) {
+    let arr = chunk.toString().split(os.EOL);
+    if(arr.length == 1){
+      this.str += arr[0];
+    } else {
+      this.push(this.str + arr[0]);
+      for(let i = 1; i < arr.length-1; i++){
+          this.push(arr[i]);
+      }
+      this.str = arr[arr.length-1];
+    }
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.str);
+    callback();
   }
 }
 
