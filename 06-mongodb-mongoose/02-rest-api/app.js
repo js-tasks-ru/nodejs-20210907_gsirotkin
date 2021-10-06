6bf4371bab185cd3b02b2abe560744ff586f983f
GeoS74 <gsirotkin@yandex.ru>
2021-10-06 20:36:05 +0500

    solution task o7-authentication-01-local

--- 06-mongodb-mongoose/02-rest-api/app.js
+++ 06-mongodb-mongoose/02-rest-api/app.js
@@ -5,25 +5,6 @@ const {categoryList} = require('./controllers/categories');
 
 const app = new Koa();
 
-const Category = require('./models/Category');
-const Product = require('./models/Product');
-app.use(require('koa-body')());
-
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    if (err.status) {
-      ctx.status = err.status;
-      ctx.body = {error: err.message};
-    } else {
-      console.error(err);
-      ctx.status = 500;
-      ctx.body = {error: 'Internal server error'};
-    }
-  }
-});
-
 const router = new Router({prefix: '/api'});
 
 router.get('/categories', categoryList);
