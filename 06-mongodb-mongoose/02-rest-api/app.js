e6face12552e68373b64e6e938f2a6c95475b21a
GeoS74 <gsirotkin@yandex.ru>
2021-10-07 21:56:52 +0500

    work on bugs

--- 06-mongodb-mongoose/02-rest-api/app.js
+++ 06-mongodb-mongoose/02-rest-api/app.js
@@ -5,6 +5,21 @@ const {categoryList} = require('./controllers/categories');
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      if (err.status) {
+        ctx.status = err.status;
+        ctx.body = {error: err.message};
+      } else {
+        console.error(err);
+        ctx.status = 500;
+        ctx.body = {error: 'Internal server error'};
+      }
+    }
+  });
+
 const router = new Router({prefix: '/api'});
 
 router.get('/categories', categoryList);
